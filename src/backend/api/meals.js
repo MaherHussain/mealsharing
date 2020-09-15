b0c321a0655e9f4fba91f163cb8a7c91e4a3cda2
Maher <maherhussain6@gmail.com>
2020-09-15 23:48:11 +0000

    hello

--- src/backend/api/meals.js
+++ src/backend/api/meals.js
@@ -11,7 +11,7 @@ router.get("/", async (request, response) => {
        throw error;
      }
 
-     response.json({ this: "is working!" });
+     response.json(results);
      // error will be an Error if one occurred during the query
      // results will contain the results of the query
      // fields will contain information about the returned results fields (if any)
